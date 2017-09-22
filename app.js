c52e03d8f6c585e69769ec7157f585b276162513
kylelingat <kylejlingat@gmail.com>
2017-09-22 10:01:50 -1000

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,30 @@
 function buildHtmlElements(responseData){
     console.log(responseData);
-    
     let title = document.createElement('h2');
     title.id = 'film-title';
     $(title).appendTo($(".film"));
     document.getElementById("film-title").innerHTML = `Title: ${responseData.title}`;
-
-    
     let episode = document.createElement("p");
     episode.id = "film-episode";
     $(episode).appendTo($(".film"))
     document.getElementById("film-episode").innerHTML = `Episode: ${responseData.episode_id}`;
-
     let director = document.createElement("p");
     director.id = "film-director";
     $(director).appendTo($(".film"));
     document.getElementById("film-director").innerHTML = `Director: ${responseData.director}`;
-    
     let date = document.createElement("p");
     date.id = "film-date";
     $(date).appendTo($(".film"));
      document.getElementById("film-date").innerHTML = `Date: ${responseData.release_date}`;
-
     let producer = document.createElement("p");
     producer.id = "film-producer";
     $(producer).appendTo($(".film"));
      document.getElementById("film-producer").innerHTML = `Producer: ${responseData.producer}`;
-
     let opening_crawl = document.createElement("p");
     opening_crawl.id = "film-crawl";
     $(opening_crawl).appendTo($(".film"));
      document.getElementById("film-crawl").innerHTML = `Opening Crawl: ${responseData.opening_crawl}`; 
 }  
-
 function getFilmData(id){
 $.ajax({
   url: "https://swapi.co/api/films",
